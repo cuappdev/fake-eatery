2934110c8d8712095e8b185f821dc382e39b1dea
Jack Greenberg <kcaj@grebneerg.com>
2020-10-29 13:20:38 -0400

    FIxed server key permissions

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 cargo build --release
-echo 1
 openssl aes-256-cbc -K $encrypted_bfd784009d19_key -iv $encrypted_bfd784009d19_iv -in server.pem.enc -out server.pem -d
-echo 2
+chmod 600 server.pem
 ssh -i server.pem -o "StrictHostKeyChecking no" root@64.227.6.156 "systemctl stop fake-eatery"
-echo 3
 scp -i server.pem -o "StrictHostKeyChecking no" ./target/release/fake-eatery root@64.227.6.156:/fake-eatery
-echo 4
 ssh -i server.pem -o "StrictHostKeyChecking no" root@64.227.6.156 "systemctl start fake-eatery"
