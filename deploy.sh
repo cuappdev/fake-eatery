e42c7f91a70bf641419558624ae9b71159798895
Jack Greenberg <kcaj@grebneerg.com>
2020-10-28 22:41:37 -0400

    No longer require manual input during CI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cargo build --release
 openssl aes-256-cbc -K $encrypted_bfd784009d19_key -iv $encrypted_bfd784009d19_iv -in server.pem.enc -out server.pem -d
-ssh -i server.pem root@64.227.6.156 "systemctl stop fake-eatery"
-scp -i server.pem ./target/release/fake-eatery root@64.227.6.156:/fake-eatery
-ssh -i server.pem root@64.227.6.156 "systemctl start fake-eatery"
+ssh -i server.pem -o "StrictHostKeyChecking no" root@64.227.6.156 "systemctl stop fake-eatery"
+scp -i server.pem -o "StrictHostKeyChecking no" ./target/release/fake-eatery root@64.227.6.156:/fake-eatery
+ssh -i server.pem -o "StrictHostKeyChecking no" root@64.227.6.156 "systemctl start fake-eatery"
